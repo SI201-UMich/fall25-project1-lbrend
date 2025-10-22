c2e300c3f2d95bd1f98e82e1661b517fd81eb61b
Brendon Lee <brendonlee@Brendons-MacBook-Air.local>
2025-10-22 04:32:59 -0400

    inspect

--- Project1.py
+++ Project1.py
@@ -6,3 +6,27 @@
 
 
 import csv
+def read_penguin_data(filename):
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
+
+        print("column1: ", data[0].keys(), )
+        print("column2: ", data[1].keys(), "\n")
+        print("sample row1: ",data[0], "\n")
+        
+        print("sample row2: ",data[1], "\n")
+        print("total rows: ", len(data))
+    return data
+
+
+
+
+
+
+def main():
+    read_penguin_data('penguins.csv')
+
+
+if __name__ == "__main__":
+    main()
